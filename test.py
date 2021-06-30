0760cef7bb896ac6112e26e6f803edaae7f3f903
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2021-06-30 17:44:58 +0530

    Basic

---
+++ test.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import smtplib
+
+
+# email = input("Enter your email address: ")
+# text = input("Enter text: ")
+# sender = "jhajhariaarnav@gmail.com"
+
+cred = pd.read_csv("credentials.txt")
+user = cred.columns[0]
+password = cred.columns[1]
+
+
+def send_mail(user, pas):
+    email = input("Enter your email address: ")
+    text = input("Enter text: ")
+    key = input("Enter key: ").upper()
+    encr, key = vigenere(text, key)
+    encr += ',' + key
+    
+
+
+
+def vigenere(text, key):
+    key_ord = []
+    for a in key:
+        key_ord.append(ord(a))
+    # print(key_ord)
+    b = 0
+    j = 0
+    final = ""
+    for i in text:
+        b = ord(i)
+        # print(i)
+        if (b < 65 or b > 90) and (b < 97 or b > 122):
+            final += i
+        if (b >= 65 and b <= 90):
+            final += chr((ord(i) - 65 + key_ord[(j % len(key))] - 65) % 26 + 65)
+            j = j + 1
+        if (b >= 97 and b <= 122):
+            final += chr((ord(i) - 97 + key_ord[(j % len(key))] - 65) % 26 + 97)
+            j = j + 1
+    return final, key
+
+send_mail(user, password)
